1c2c60ae553b8f7592a6d2eb7637e2af3b91cb73
bchen <bunbun.d.chan@gmail.com>
2021-08-17 23:14:37 +0800

    basis

---
+++ Basis.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+Basis = [
+    {
+        'name': 'Square',
+        'unit_cell': [[0,0]],
+        'vector': [[1,0],[0,1]]
+    },
+    {
+        'name': 'Triangle_YC',
+        'unit_cell': [[0,0]],
+        'vector': [[np.sqrt(3)/2, 1/2],[0,1]]
+    },
+]
