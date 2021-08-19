dc922dbe2a14e1d380abfc39eff43b59ef01d25a
bchen <bunbun.d.chan@gmail.com>
2021-08-19 23:04:27 +0800

    honeycomb_YC

--- Basis.py
+++ Basis.py
@@ -11,4 +11,9 @@ Basis = [
         'unit_cell': [[0,0]],
         'vector': [[np.sqrt(3)/2, 1/2],[0,1]]
     },
+    {
+        'name': 'Honeycomb_YC',
+        'unit_cell': [[0,0], [0,np.sqrt(3)/3]],
+        'vector': [[1,0], [1/2,np.sqrt(3)/2]],    
+    },
 ]
