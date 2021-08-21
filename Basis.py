44f922085fa5aa7c3d7f1d114165404fe95657af
bchen <bunbun.d.chan@gmail.com>
2021-08-21 13:53:59 +0800

    Kagome

--- Basis.py
+++ Basis.py
@@ -4,16 +4,21 @@ Basis = [
     {
         'name': 'Square',
         'unit_cell': [[0,0]],
-        'vector': [[1,0],[0,1]]
+        'vector': [[1,0],[0,1]],
     },
     {
         'name': 'Triangle_YC',
         'unit_cell': [[0,0]],
-        'vector': [[np.sqrt(3)/2, 1/2],[0,1]]
+        'vector': [[np.sqrt(3)/2, 1/2],[0,1]],
     },
     {
         'name': 'Honeycomb_YC',
         'unit_cell': [[0,0], [0,np.sqrt(3)/3]],
         'vector': [[1,0], [1/2,np.sqrt(3)/2]],    
+    },
+    {
+        'name': 'Kagome_YC',
+        'unit_cell': [[0,0], [-np.sqrt(3)/4,1/4], [0,1/2]],
+        'vector': [[np.sqrt(3)/2, -1/2], [0,1]],
     },
 ]
